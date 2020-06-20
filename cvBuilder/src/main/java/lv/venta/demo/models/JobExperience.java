8fa457d0480e98cbb0ea680b4e129abd6dc73e3f
Emīls Riekstiņš <emilsrie@gmail.com>
2020-06-21 01:45:42 +0300

    Completed models. Subejct to change. Date as Calendar? Language levels as enums?

--- cvBuilder/src/main/java/lv/venta/demo/models/JobExperience.java
+++ cvBuilder/src/main/java/lv/venta/demo/models/JobExperience.java
@@ -1,17 +1,131 @@
 package lv.venta.demo.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 //Job experience for person
 @Entity
 @Table(name="JobExperienceTable")
 public class JobExperience {
-	String job_title;
-	String job_info;
-	String company_name;
-	String location;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private int id;
 	
-	//Starting date
-	//Ending date
+	@Column
+	private String job_title;
+	
+	@Column
+	private String job_info;
+
+	@Column
+	private String company_name;
+	
+	@Column
+	private String location;
+	
+	@Column
+	private Date startDate;
+	
+	@Column
+	private Date endDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID")
+	private CV cv;
+
+	public JobExperience() {
+	}
+	
+	public JobExperience(String job_title, String job_info, String company_name, String location, Date startDate,
+			Date endDate, CV cv) {
+		super();
+		this.job_title = job_title;
+		this.job_info = job_info;
+		this.company_name = company_name;
+		this.location = location;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.cv = cv;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getJob_title() {
+		return job_title;
+	}
+
+	public void setJob_title(String job_title) {
+		this.job_title = job_title;
+	}
+
+	public String getJob_info() {
+		return job_info;
+	}
+
+	public void setJob_info(String job_info) {
+		this.job_info = job_info;
+	}
+
+	public String getCompany_name() {
+		return company_name;
+	}
+
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		//Validation. Maybe remake to Calendar
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		//Validation. Maybe remake to Calendar
+		this.endDate = endDate;
+	}
+
+	public CV getCv() {
+		return cv;
+	}
+
+	public void setCv(CV cv) {
+		this.cv = cv;
+	}
+
+	@Override
+	public String toString() {
+		return "JobExperience [job_title=" + job_title + ", job_info=" + job_info + ", company_name=" + company_name
+				+ ", location=" + location + ", startDate=" + startDate + ", endDate=" + endDate + ", cv=" + cv + "]";
+	}
 }

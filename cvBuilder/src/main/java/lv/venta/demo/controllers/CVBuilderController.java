66d1762e8bdda0e0b719afc87fd05d49239cef5d
Janis <Janis@DESKTOP-1SJ0KIG>
2020-06-21 12:44:31 +0300

    a few basic changes nthn important

--- cvBuilder/src/main/java/lv/venta/demo/controllers/CVBuilderController.java
+++ cvBuilder/src/main/java/lv/venta/demo/controllers/CVBuilderController.java
@@ -1,5 +1,6 @@
 package lv.venta.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 
 import lv.venta.demo.models.CV;
+import lv.venta.demo.services.impl.ServiceImpl;
 
 @Controller
 @RequestMapping("/cvBuilder")
 public class CVBuilderController
 {
+	@Autowired
+	ServiceImpl serviceImpl;
 	
 	@GetMapping("/build")
 	public String makeCVGet(CV cv)

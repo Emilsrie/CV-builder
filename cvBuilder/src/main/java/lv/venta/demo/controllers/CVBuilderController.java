4c6ea6185e7eb7df1b91ca2de6af629e63a82a39
Janis <Janis@DESKTOP-1SJ0KIG>
2020-06-21 12:33:15 +0300

    Update for controller

--- cvBuilder/src/main/java/lv/venta/demo/controllers/CVBuilderController.java
+++ cvBuilder/src/main/java/lv/venta/demo/controllers/CVBuilderController.java
@@ -5,7 +5,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import javax.validation.Valid;
 
 import lv.venta.demo.models.CV;
 

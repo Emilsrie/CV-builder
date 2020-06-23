7f25696e45316e98931058e3abfeac17aa3b4376
Emīls Riekstiņš <emilsrie@gmail.com>
2020-06-23 16:18:54 +0300

    Deleted unneeded imports

--- cvBuilder/src/main/java/lv/venta/demo/services/impl/ServiceImpl.java
+++ cvBuilder/src/main/java/lv/venta/demo/services/impl/ServiceImpl.java
@@ -3,16 +3,10 @@ package lv.venta.demo.services.impl;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

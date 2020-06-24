96fa0fb457e2d86af223ee1c66bca48aaefe19b5
Emīls Riekstiņš <emilsrie@gmail.com>
2020-06-24 20:10:34 +0300

    Cleaning code

--- cvBuilder/src/main/java/lv/venta/demo/CvBuilderApplication.java
+++ cvBuilder/src/main/java/lv/venta/demo/CvBuilderApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CvBuilderApplication
-{
+public class CvBuilderApplication {
 
 	public static void main(String[] args) {	
 		//comment 

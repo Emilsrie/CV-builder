4f09f0472de8fb7bdab9d775d94f9c4e54e63b01
Emīls Riekstiņš <emilsrie@gmail.com>
2020-06-24 20:16:46 +0300

    Deleted comment

--- cvBuilder/src/main/java/lv/venta/demo/CvBuilderApplication.java
+++ cvBuilder/src/main/java/lv/venta/demo/CvBuilderApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CvBuilderApplication {
 
 	public static void main(String[] args) {	
-		//comment 
 		SpringApplication.run(CvBuilderApplication.class, args);
 	}
 
